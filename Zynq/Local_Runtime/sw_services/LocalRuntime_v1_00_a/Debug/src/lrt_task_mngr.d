ebb32c9a1c1439946527c892ba27bd0499c20b0d
Yaset Oliva <yaset.oliva@insa-rennes.fr>
2013-02-05 11:03:03 +0000

    For use as SDK library.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/compa/trunk@129 4d43d934-8427-4298-9e40-070a8115aafb

---
+++ Zynq/Local_Runtime/sw_services/LocalRuntime_v1_00_a/Debug/src/lrt_task_mngr.d
@@ -0,0 +1,8 @@
+src/lrt_task_mngr.d: ../src/lrt_task_mngr.c ../src/lrt_definitions.h \
+ ../src/lrt_cfg.h ../src/lrt_cpu_cfg.h
+
+../src/lrt_definitions.h:
+
+../src/lrt_cfg.h:
+
+../src/lrt_cpu_cfg.h:

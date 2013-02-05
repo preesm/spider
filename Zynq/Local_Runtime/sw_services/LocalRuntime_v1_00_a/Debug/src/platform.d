c18e55b7302cb4a6deb2430f39ff952b84c2f412
Yaset Oliva <yaset.oliva@insa-rennes.fr>
2013-02-05 10:48:11 +0000

    For use as SDK library
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/compa/trunk@119 4d43d934-8427-4298-9e40-070a8115aafb

---
+++ Zynq/Local_Runtime/sw_services/LocalRuntime_v1_00_a/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d: ../src/platform.c \
+ C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xparameters.h \
+ C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xil_cache.h \
+ C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/mb_interface.h \
+ C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xbasic_types.h \
+ C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xil_types.h \
+ C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xparameters.h:
+
+C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xil_cache.h:
+
+C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/mb_interface.h:
+
+C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xbasic_types.h:
+
+C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xil_types.h:
+
+C:/work/Xilinx/projects/software/local_runtime_bsp_0/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:

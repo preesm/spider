21e50bb4ed4821188fc9411c9a952d48e3ef9292
Yaset Oliva <yaset.oliva@insa-rennes.fr>
2013-07-05 07:26:10 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/compa/trunk@197 4d43d934-8427-4298-9e40-070a8115aafb

--- Zynq/Global_Runtime/flow-shop/src/test_graph_trans.cpp
+++ Zynq/Global_Runtime/flow-shop/src/test_graph_trans.cpp
@@ -10,7 +10,7 @@
 
 #include "tools/DotWriter.h"
 
-
+#include "transformations/PiCSDFTransformer/PiCSDFTransformer.h"
 
 
 #define NB_EDGES					4
@@ -18,33 +18,53 @@
 
 #define PRINT_GRAPH					1
 #define CSDAG_FILE_PATH 			"csdag.gv"
+#define SRDAG_FILE_PATH 			"srdag.gv"
 
 
+void create_graph_1(PiCSDFGraph* graph){
+	CSDAGVertex *vertexA = graph->addVertex("A");
+	CSDAGVertex *vertexB = graph->addVertex("B");
 
+	graph->addEdge(vertexA, "3", vertexB, "2", "0");
+}
+
+
+void create_graph_2(PiCSDFGraph* graph){
+	CSDAGVertex *vertexA = graph->addVertex("A");
+	CSDAGVertex *vertexB = graph->addVertex("B");
+	CSDAGVertex *vertexC = graph->addVertex("C");
+	CSDAGVertex *vertexD = graph->addVertex("D");
+
+	graph->addEdge(vertexA, "3", vertexB, "2", "1");
+	graph->addEdge(vertexA, "3", vertexC, "2", "0");
+	graph->addEdge(vertexB, "2", vertexD, "4", "0");
+	graph->addEdge(vertexC, "2", vertexD, "4", "0");
+}
 
 
 int main(int nArgs, char** pArgs)
 {
-	DotWriter 		dotWriter;
+	DotWriter 			dotWriter;
+	PiCSDFTransformer	trans;
 
 	// Creating PiSDF graph.
 	PiCSDFGraph inputGraph;
-	CSDAGVertex *vertexA = inputGraph.addVertex("A");
-	CSDAGVertex *vertexB = inputGraph.addVertex("B");
-	CSDAGVertex *vertexC = inputGraph.addVertex("C");
-	CSDAGVertex *vertexD = inputGraph.addVertex("D");
 
-	inputGraph.addEdge(vertexA, "3", vertexB, "2", "1");
-	inputGraph.addEdge(vertexA, "3", vertexC, "2", "0");
-	inputGraph.addEdge(vertexB, "2", vertexD, "4", "0");
-	inputGraph.addEdge(vertexC, "2", vertexD, "4", "0");
+//	create_graph_1(&inputGraph);
+	create_graph_2(&inputGraph);
 
 	// Printing the PiSDF graph.
 #if PRINT_GRAPH
-	dotWriter.write(&inputGraph, CSDAG_FILE_PATH, 1);
+	dotWriter.write((PiCSDFGraph*)&inputGraph, CSDAG_FILE_PATH, 1);
 #endif
 
 	// Transforming PiSDF to SrDAG if schedulable.
+	SRDAGGraph outputGraph;
+	trans.transform(&inputGraph, &outputGraph, (Architecture*)0);
+
 
 	// Printing the SrDAG graph.
+#if PRINT_GRAPH
+	dotWriter.write((SRDAGGraph*)&outputGraph, SRDAG_FILE_PATH, 1);
+#endif
 }

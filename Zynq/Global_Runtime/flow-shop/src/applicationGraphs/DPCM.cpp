602f48b9d0a0c730d40e89f6fe461252e39eeef8
Yaset Oliva <yaset.oliva@insa-rennes.fr>
2013-09-25 14:12:44 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/compa/trunk@231 4d43d934-8427-4298-9e40-070a8115aafb

---
+++ Zynq/Global_Runtime/flow-shop/src/applicationGraphs/DPCM.cpp
@@ -0,0 +1,98 @@
+/*
+ * DPCM.cpp
+ *
+ *  Created on: 5 juil. 2013
+ *      Author: yoliva
+ */
+#include <string.h>
+#include "DPCM.h"
+#include "../graphs/PiCSDF/PiCSDFGraph.h"
+
+
+#define H_VALUE	3
+#define W_VALUE	4
+#define B_VALUE	1
+#define N_VALUE	1
+
+
+void createPiCSDF_DPCM_1(PiCSDFGraph* graph){
+	// Vertices.
+	CSDAGVertex *vertexRead = graph->addVertex("Read");
+	CSDAGVertex *vertexRasterToMB = graph->addVertex("RasterToMB");
+	CSDAGVertex *vertexDPCM = graph->addVertex("DPCM");
+	CSDAGVertex *vertexMBToRaster = graph->addVertex("MBToRaster");
+	CSDAGVertex *vertexWrite = graph->addVertex("Write");
+
+	// Configuration vertices.
+	CSDAGVertex *vertexSetH = graph->addConfigVertex("SetH");
+	CSDAGVertex *vertexSetW = graph->addConfigVertex("SetW");
+	CSDAGVertex *vertexSetB = graph->addConfigVertex("SetB");
+	CSDAGVertex *vertexSetN = graph->addConfigVertex("SetN");
+
+	// Variables.
+	globalParser.addVariable("H", 1);
+	globalParser.addVariable("W", 1);
+	globalParser.addVariable("B", 1);
+	globalParser.addVariable("N", 1);
+
+	// Parameters.
+	PiCSDFParameter* paramH = graph->addParameter("H", "H");
+	PiCSDFParameter* paramW = graph->addParameter("W", "W");
+	PiCSDFParameter* paramB = graph->addParameter("B", "B");
+	PiCSDFParameter* paramN = graph->addParameter("N", "N");
+
+	// Configuration input ports.
+	graph->addConfigPort(vertexRead, paramH, 0);
+	graph->addConfigPort(vertexRead, paramW, 0);
+	graph->addConfigPort(vertexRead, paramB, 0);
+	graph->addConfigPort(vertexRasterToMB, paramH, 0);
+	graph->addConfigPort(vertexRasterToMB, paramW, 0);
+	graph->addConfigPort(vertexRasterToMB, paramB, 0);
+	graph->addConfigPort(vertexDPCM, paramN, 0);
+	graph->addConfigPort(vertexDPCM, paramB, 0);
+	graph->addConfigPort(vertexMBToRaster, paramH, 0);
+	graph->addConfigPort(vertexMBToRaster, paramW, 0);
+	graph->addConfigPort(vertexMBToRaster, paramB, 0);
+	graph->addConfigPort(vertexWrite, paramH, 0);
+	graph->addConfigPort(vertexWrite, paramW, 0);
+	graph->addConfigPort(vertexWrite, paramB, 0);
+
+	// Configuration output ports.
+	graph->addConfigPort(vertexSetH, paramH, 1);
+	graph->addConfigPort(vertexSetW, paramW, 1);
+	graph->addConfigPort(vertexSetB, paramB, 1);
+	graph->addConfigPort(vertexSetN, paramN, 1);
+
+	// Edges.
+	graph->addEdge(vertexRead, "W*H*B", vertexRasterToMB, "W*H*B", "0");
+	graph->addEdge(vertexRasterToMB, "W*H*B", vertexDPCM, "H*B/N", "0");
+//	graph->addEdge(vertexDPCM, "H*B/N", vertexDPCM, "H*B/N", "H*B");
+	graph->addEdge(vertexDPCM, "H*B/N", vertexMBToRaster, "W*H*B", "0");
+	graph->addEdge(vertexMBToRaster, "W*H*B", vertexWrite, "W*H*B", "0");
+}
+
+
+int executeConfigActorDPCM(CSDAGVertex* vertex){
+	if(strcmp(vertex->getName(), "SetH") == 0)
+		return H_VALUE;
+	if(strcmp(vertex->getName(), "SetW") == 0)
+		return W_VALUE;
+	if(strcmp(vertex->getName(), "SetB") == 0)
+		return B_VALUE;
+//	if(strcmp(vertex->getName(), "SetN") == 0)
+	return N_VALUE;
+}
+
+
+void resolvePiCSDFParams(PiCSDFGraph* graph){
+	for (int i = 0; i < graph->getNbConfigOutPorts(); i++) {
+		// Getting the output port.
+		PiCSDFConfigPort *outPort = graph->getConfigOutPort(i);
+
+		// Retrieving the value from the actor's execution.
+		int value = executeConfigActorDPCM(outPort->vertex);
+
+		// Updating the corresponding parameter' value.
+		((variable*)(outPort->parameter->expression[0].elt_union.var))->value = value;
+	}
+}
---
+++ Zynq/Global_Runtime/flow-shop/src/applicationGraphs/mpeg_part2.h
@@ -0,0 +1,17 @@
+/*
+ * mpeg_part2.h
+ *
+ *  Created on: 10 sept. 2013
+ *      Author: yoliva
+ */
+
+#ifndef MPEG_PART2_H_
+#define MPEG_PART2_H_
+
+#include "../graphs/PiCSDF/PiCSDFGraph.h"
+
+void create_PiSDF_mpeg_part2(PiCSDFGraph* graph);
+
+//void resolvePiCSDFParams(PiCSDFGraph* graph);
+
+#endif /* MPEG_PART2_H_ */

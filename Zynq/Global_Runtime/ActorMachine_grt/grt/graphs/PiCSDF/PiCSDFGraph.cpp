34b064e2185ac533ab148d0f9ce4c8d8476d23c6
Yaset Oliva <yaset.oliva@insa-rennes.fr>
2013-09-25 14:15:19 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/compa/trunk@232 4d43d934-8427-4298-9e40-070a8115aafb

---
+++ Zynq/Global_Runtime/ActorMachine_grt/grt/graphs/PiCSDF/PiCSDFGraph.cpp
@@ -0,0 +1,160 @@
+/*
+ * PiCSDFGraph.cpp
+ *
+ *  Created on: 12 juin 2013
+ *      Author: yoliva
+ */
+
+#include <string.h>
+#include "PiCSDFGraph.h"
+
+
+/**
+ Adding an edge to the graph. Vertices and edges must be added in topological order.
+
+ @param source: The source vertex of the edge
+ @param production: number of tokens (chars) produced by the source
+ @param sink: The sink vertex of the edge
+ @param consumption: number of tokens (chars) consumed by the sink
+ @param initial_tokens: number of initial tokens.
+ @return the created edge
+*/
+PiCSDFEdge* PiCSDFGraph::addEdge(CSDAGVertex* source, const char* production, CSDAGVertex* sink, const char* consumption, const char* delay){
+	PiCSDFEdge* edge = NULL;
+	if(nbEdges < MAX_CSDAG_EDGES){
+		edge = &edges[nbEdges];
+		edge->setBase(this);
+		edge->setSource(source);
+		edge->setProduction(production);
+		edge->setSink(sink);
+		edge->setConsumption(consumption);
+		edge->setDelay(delay);
+		nbEdges++;
+	}
+	else{
+		// Adding an edge while the graph is already full
+		exitWithCode(1001);
+	}
+	return edge;
+}
+
+/**
+ Adding a configuration vertex to the graph.
+
+ @param vertexName: the name of the new vertex
+ @return the new vertex
+*/
+CSDAGVertex* PiCSDFGraph::addConfigVertex(const char* vertexName){
+	CSDAGVertex* vertex = NULL;
+	if(nbConfigVertices < MAX_PISDF_CONFIG_VERTICES){
+		vertex = &configVertices[nbConfigVertices];
+		vertex->setBase(this);
+		vertex->setName(vertexName);
+		nbConfigVertices++;
+	}
+	else{
+		// Adding a vertex while the graph is already full
+		exitWithCode(1000);
+	}
+	return vertex;
+}
+
+/**
+ * Get number of configuration vertices.
+ */
+int PiCSDFGraph::getNbConfigVertices(){
+	return nbConfigVertices;
+}
+
+
+/**
+ Adding a configuration port to the graph
+
+ @param vertex: pointer to the vertex connected to the port.
+ 	 	param:	pointer to the parameter connected to the port.
+ 	 	dir:	port direction. 0:input, 1:output.
+
+ @return the new configuration port.
+*/
+PiCSDFConfigPort* PiCSDFGraph::addConfigPort(CSDAGVertex* vertex, PiCSDFParameter* param, int dir){
+	PiCSDFConfigPort* configPort = NULL;
+	if(dir == 0) // Input port.
+	{
+		if(nbConfigInPorts < MAX_PISDF_CONFIG_PORTS); //TODO handle the error. exitWithCode()
+		configPort = &configInPorts[nbConfigInPorts];
+		nbConfigInPorts++;
+	}
+	else // Output port.
+	{
+		if(nbConfigOutPorts < MAX_PISDF_CONFIG_PORTS); //TODO handle the error. exitWithCode()
+		configPort = &configOutPorts[nbConfigOutPorts];
+		nbConfigOutPorts++;
+	}
+
+	configPort->vertex = vertex;
+	configPort->parameter = param;
+	configPort->direction = dir;
+
+	return configPort;
+}
+
+
+/*
+ * Getting a configuration input port.
+ */
+PiCSDFConfigPort* PiCSDFGraph::getConfigInPort(int index){
+	return &configInPorts[index];
+}
+
+
+/*
+ * Getting a configuration output port.
+ */
+PiCSDFConfigPort* PiCSDFGraph::getConfigOutPort(int index){
+	return &configOutPorts[index];
+}
+
+
+/**
+ * Get number of configuration input ports.
+ */
+int PiCSDFGraph::getNbConfigInPorts(){
+	return nbConfigInPorts;
+}
+
+
+/**
+ * Get number of configuration input ports.
+ */
+int PiCSDFGraph::getNbConfigOutPorts(){
+	return nbConfigOutPorts;
+}
+
+
+/**
+ Adding a parameter to the graph
+
+ @param expression: //expression defining the parameter's value.
+
+ @return the new parameter.
+*/
+PiCSDFParameter* PiCSDFGraph::addParameter(const char* name, const char* expression){
+	PiCSDFParameter* parameter = NULL;
+	if(nbParameters < MAX_PISDF_CONFIG_PORTS){
+		parameter = &parameters[nbParameters];
+		strcpy(parameter->name, name);
+		// Parsing the expression
+		globalParser.parse(expression, parameter->expression);
+		nbParameters++;
+	}
+	else{
+		//TODO handle the error.
+//		exitWithCode(1000);
+	}
+	return parameter;
+}
+
+
+/**
+ * Getting the number of vertices.
+ */
---
+++ Zynq/Global_Runtime/ActorMachine_grt/grt/graphs/PiCSDF/PiCSDFGraph.h
@@ -0,0 +1,232 @@
+/*
+ * PiCSDFGraph.h
+ *
+ *  Created on: 12 juin 2013
+ *      Author: yoliva
+ */
+
+#ifndef PICSDFGRAPH_H_
+#define PICSDFGRAPH_H_
+
+#include "../CSDAG/CSDAGGraph.h"
+#include "PiCSDFEdge.h"
+
+
+/*
+ * This data types may become classes later.
+ */
+// Parameter
+typedef struct PiCSDFParameter{
+	char 	name[MAX_VERTEX_NAME_SIZE];
+	abstract_syntax_elt expression[REVERSE_POLISH_STACK_MAX_ELEMENTS+1]; //expression defining the parameter's value.
+}PiCSDFParameter;
+
+
+// ConfigPort
+typedef struct PiCSDFConfigPort{
+	CSDAGVertex* 		vertex;
+	PiCSDFParameter* 	parameter;
+	int 				direction; // 0:input, 1:output.
+}PiCSDFConfigPort;
+
+
+class PiCSDFGraph : public CSDAGGraph{
+	private:
+
+		/*
+		 * Table of configuration actors.
+		 */
+		CSDAGVertex configVertices[MAX_PISDF_CONFIG_VERTICES];
+		int nbConfigVertices;
+
+		/**
+		 table of PiCSDF edges
+		*/
+		PiCSDFEdge edges[MAX_CSDAG_EDGES];
+
+		/*
+		 * Table of configuration input ports.
+		 */
+		PiCSDFConfigPort configInPorts[MAX_PISDF_CONFIG_PORTS];
+		int nbConfigInPorts;
+
+		/*
+		 * Table of configuration output ports.
+		 */
+		PiCSDFConfigPort configOutPorts[MAX_PISDF_CONFIG_PORTS];
+		int nbConfigOutPorts;
+
+		/*
+		 * Table of parameters.
+		 */
+		PiCSDFParameter parameters[MAX_PISDF_CONFIG_PORTS];
+		int nbParameters;
+
+	public:
+		PiCSDFGraph():CSDAGGraph(){nbConfigVertices	= 0;
+								   nbConfigInPorts	= 0;
+								   nbConfigOutPorts	= 0;
+								   nbParameters		= 0;};
+
+		/**
+		 Adding an edge to the graph. Vertices and edges must be added in topological order.
+
+		 @param source: The source vertex of the edge
+		 @param production: number of tokens (chars) produced by the source
+		 @param sink: The sink vertex of the edge
+		 @param consumption: number of tokens (chars) consumed by the sink
+		 @param delay: number of initial tokens.
+		 @return the created edge
+		*/
+		PiCSDFEdge* addEdge(CSDAGVertex* source, const char* production, CSDAGVertex* sink, const char* consumption, const char* delay);
+
+
+		/**
+		 Gets the edge at the given index
+
+		 @param index: index of the edge in the edge table
+		 @return edge
+		*/
+		PiCSDFEdge* getEdge(int index);
+
+
+		/**
+		 Gets the input edges of a given vertex
+
+		 @param vertex: input vertex
+		 @param output: table to store the edges
+		 @return the number of input edges
+		*/
+		int getInputEdges(CSDAGVertex* vertex, PiCSDFEdge** output);
+
+
+		/**
+		 Adding a configuration vertex to the graph.
+
+		 @param vertexName: the name of the new vertex
+		 @return the new vertex
+		*/
+		CSDAGVertex* addConfigVertex(const char* vertexName);
+
+
+		/**
+		 * Get number of configuration vertices.
+		 */
+		int getNbConfigVertices();
+
+
+		/*
+		 * Get a configuration vertex
+		 */
+		CSDAGVertex* getConfigVertex(const int index);
+
+		/**
+		 Adding a configuration port to the graph
+
+		 @param vertex: pointer to the vertex connected to the port.
+		 	 	param:	pointer to the parameter connected to the port.
+		 	 	dir:	port direction. 0:input, 1:output.
+
+		 @return the new configuration port.
+		*/
+		PiCSDFConfigPort* addConfigPort(CSDAGVertex* vertex, PiCSDFParameter* param, int dir);
+
+
+		/*
+		 * Get a configuration input port
+		 */
+		PiCSDFConfigPort* getConfigInPort(int index);
+
+
+		/*
+		 * Get a configuration output port
+		 */
+		PiCSDFConfigPort* getConfigOutPort(int index);
+
+
+		/**
+		 * Getter of the number of configuration input ports.
+		 */
+		int getNbConfigInPorts();
+
+
+
+		/**
+		 * Getter of the number of configuration output ports.
+		 */
+		int getNbConfigOutPorts();
+
+
+
+		/**
+		 Adding a parameter to the graph
+
+		 @param expression: //expression defining the parameter's value.
+
+		 @return the new parameter.
+		*/
+		PiCSDFParameter* addParameter(const char* name, const char* expression);
+
+		int getNbParameters();
+
+		PiCSDFParameter* getParameter(const int index);
+};
+
+
+
+/**
+ Gets the edge at the given index
+
+ @param index: index of the edge in the edge list
+ @return edge
+*/
+inline PiCSDFEdge* PiCSDFGraph::getEdge(int index){
+	return &edges[index];
+}
+
+
+/**
+ Gets the input edges of a given vertex
+
+ @param vertex: input vertex
+ @param output: table to store the edges
+ @return the number of input edges
+*/
+inline int PiCSDFGraph::getInputEdges(CSDAGVertex* vertex, PiCSDFEdge** output){
+	int nbEdges = 0;
+	for(int i=0; i<this->nbEdges; i++){
+		PiCSDFEdge* edge = &edges[i];
+		CSDAGVertex* sink = edge->getSink();
+		if(sink == vertex){
+			output[nbEdges] = edge;
+			nbEdges++;
+		}
+	}
+	return nbEdges;
+}
+
+
+/**
+ Gets the configuration vertex at the given index.
+*/
+inline CSDAGVertex* PiCSDFGraph::getConfigVertex(const int index){
+	return &configVertices[index];
+}
+
+
+/**
+ Gets the number of parameters.
+*/
+inline int PiCSDFGraph::getNbParameters(){
+	return nbParameters;
+}
+
+
+/**
+ Gets the parameter at the given index.
+*/
+inline PiCSDFParameter* PiCSDFGraph::getParameter(const int index){
+	return &parameters[index];
+}
+
+#endif /* PICSDFGRAPH_H_ */

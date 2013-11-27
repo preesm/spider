433d1adcac1dc4ff4fe947a056568ac235221378
Yaset Oliva <yaset.oliva@insa-rennes.fr>
2013-11-27 10:39:30 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/compa/trunk@267 4d43d934-8427-4298-9e40-070a8115aafb

--- Zynq/Local_Runtime/ActorMachine_lrt/platform_desktop_win/types.h
+++ Zynq/Local_Runtime/ActorMachine_lrt/platform_desktop_win/types.h
@@ -8,19 +8,19 @@
 #ifndef TYPES_H_
 #define TYPES_H_
 
-#define TRUE 			1
-#define FALSE 			0
-
+//#define TRUE 			1
+//#define FALSE 			0
+//
 typedef unsigned char 	BOOLEAN;
-
-typedef char			INT8;
-typedef short			INT16;
-typedef int			 	INT32;
-typedef long long	 	INT64;
-
-typedef unsigned char 	UINT8;
-typedef unsigned short	UINT16;
+//
+//typedef char			INT8;
+//typedef short			INT16;
+//typedef int			 	INT32;
+//typedef long long	 	INT64;
+//
+//typedef unsigned char 	UINT8;
+//typedef unsigned short	UINT16;
 typedef unsigned int	UINT32;
-typedef long long unsigned	UINT64;
+//typedef long long unsigned	UINT64;
 
 #endif /* TYPES_H_ */

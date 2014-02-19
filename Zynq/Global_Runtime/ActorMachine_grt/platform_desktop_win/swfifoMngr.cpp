0897eb4b91ea4ac4aa156f37b05922481b695371
Yaset Oliva <yaset.oliva@insa-rennes.fr>
2014-02-19 15:12:49 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/compa/trunk@393 4d43d934-8427-4298-9e40-070a8115aafb

--- Zynq/Global_Runtime/ActorMachine_grt/platform_desktop_win/swfifoMngr.cpp
+++ Zynq/Global_Runtime/ActorMachine_grt/platform_desktop_win/swfifoMngr.cpp
@@ -40,7 +40,7 @@
 #include <platform.h>
 #include "sharedMem.h"
 #include "swfifoMngr.h"
-#include <rtErrorCodes.h>
+//#include <rtErrorCodes.h>
 
 /* MACROs */
 #define FIFO_RD_IX_OFFSET		0		// Offsets in number of 32-bits words.

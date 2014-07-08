aa76a0e55fbafbee12799feb0ca29fa8c4d364ac
Julien Heulot <jheulot@insa-rennes.fr>
2014-07-08 10:25:24 +0200

    New error handling

--- GRT/grt/tools/SchedulingError.h
+++ GRT/grt/tools/SchedulingError.h
@@ -46,92 +46,105 @@
  * @author mpelcat
  */
 
+#define MAX_ERROR_COUNT 100
+
+static const char* errorString[MAX_ERROR_COUNT] = {
+	/* 2000*/ "Name too large",
+	/* 2001*/ "Impossible to create new element",
+	/* 2002*/ "Removing element not present",
+	/* 2003*/ "Index out of bound",
+	/* 2004*/ "Access not set element",
+	/* 2005*/ "Set already set element",
+	/* 2006*/ "Reset not set element",
+
+//	/* 0*/ "Adding a vertex while the graph is already full",
+//	/* 1*/ "Adding an edge while the graph is already full",
+//	/* 2*/ "The edge source repetition number outsizes the maximal pattern size",
+//	/* 3*/ "Two input edges set different repetition numbers to their common sink",
+//	/* 4*/ "The edge sink repetition number outsizes the maximal pattern size",
+//	/* 5*/ "The CSDAG to SRDAG transformation led to an unbalanced number of produced and consumed token for an edge",
+//	/* 6*/ "SRDAG edge creation led to an unbalanced number of produced and consumed token for an edge",
+//	/* 7*/ "Removing an edge from an empty graph",
+//	/* 8*/ "Adding a slave when the slave table is full",
+//	/* 9*/ "Adding a slave type with a too big index",
+//	/*10*/ "Adding a production pattern in a full table",
+//	/*11*/ "Adding a consumption pattern in a full table",
+//	/*12*/ "Adding a variable in an already full variable table",
+//	/*13*/ "Too long expression",
+//	/*14*/ "Early end of expression after delimiter",
+//	/*15*/ "User entered a not existing operator like \"//\"",
+//	/*16*/ "Garbage at the end of the expression ",
+//	/*17*/ "Unwanted character in expression",
+//	/*18*/ "",
+//	/*19*/ "Unexpected end of expression",
+//	/*20*/ "Expression syntax error",
+//	/*21*/ "Operator is not recognized",
+//	/*22*/ "Operator is not recognized",
+//	/*23*/ "Unknown variable",
+//	/*24*/ "Unknown timing",
+//	/*25*/ "Trying to allocate a buffer on a full memory",
+//	/*26*/ "Adding an expression element in a full stack",
+//	/*27*/ "Searching a non existing variable while creating a stack",
+//	/*28*/ "Unrecognized token",
+//	/*29*/ "Adding an expression element in a full queue",
+//	/*30*/ "Removing an expression element from an empty queue",
+//	/*31*/ "Popping an expression element from an empty stack",
+//	/*32*/ "Parser parenthesis mismatch",
+//	/*33*/ "Parser parenthesis mismatch. Left parenthesis missing",
+//	/*34*/ "",
+//	/*35*/ "Peeking an expression element from an empty stack",
+//	/*36*/ "Failure in RPN parsing",
+//	/*37*/ "Expression syntax error: wrong variable assignment",
+//	/*38*/ "Problem with variable assignment",
+//	/*39*/ "Adding a parameter pattern in a full table",
+//	/*40*/ "Adding a parameter in a full table",
+//	/*41*/ "Incorrect index",
+//	/*42*/ "Setting an improper data rate",
+//	/*43*/ "Removing a slave from an empty architecture",
+//	/*44*/ "The data rate does not fit in table dynamic. Too low rate.",
+//	/*45*/ "Error in xmlParser",
+//	/*46*/ "Error in Timer",
+//	/*47*/ "Too many Input for a Vertex",
+//	/*48*/ "Too many Output for a Vertex",
+//	/*49*/ "Schedule Check Failed",
+//	/*50*/ "Production/Consumption Pattern too long",
+//	/*51*/ "Parameter Pattern too long",
+//	/*52*/ "Parameters cannot have a pattern",
+//	/*53*/ "Try to activate more slaves that the Architecture got",
+//	/*54*/ "No more available subgraphs.",
+//	/*55*/ "Too many parameters.",
+//	/*56*/ "Root vertex not set.",
+//	/*57*/ "Too many parameters.",
+//	/*58*/ "Too many actor machines.",
+//	/*59*/ "Too many actors.",
+//	/*60*/ "Too many FIFOs.",
+//	/*61*/ "Graph already executed.",
+//	/*62*/ "No executable vertices found.",
+//	/*63*/ "Multiple SrDAG vertices reference a single PiSDF configure vertex.",
+//	/*64*/ "Matching not found for unplugged vertex when merging SrDAG.",
+//	/*65*/ "Vertex was deleted.",
+//	/*66*/ "Invalid production/consumption value.",
+//	/*67*/ "Unexpected amount of data received.",
+//	/*68*/ "Unexpected data values received.",
+//	/*69*/ "Top's subgraph is missing.",
+//	/*70*/ "Top's root vertex is missing.",
+//	/*71*/ "Local SDF graph was empty.",
+//	/*72*/ "MAX_FILE_NAME_SIZE too small.",
+//	/*73*/ "MAX_SLAVE_NAME_SIZE too small.",
+//	/*74*/ "EXPR_LEN_MAX too small.",
+//	/*75*/ "MAX_VERTEX_NAME_SIZE too small.",
+//	/*76*/ "List too small",
+};
+
 /**
  Exits returning a code representing the current error
 
  @param exitCode: code representing the current error
 */
-static void exitWithCode(int exitCode){
-
-	// ExitCodes:
-
-	// 1000: Adding a vertex while the graph is already full
-	// 1001: Adding an edge while the graph is already full
-	// 1002: The edge source repetition number outsizes the maximal pattern size
-	// 1003: Two input edges set different repetition numbers to their common sink
-	// 1004: The edge sink repetition number outsizes the maximal pattern size
-	// 1005: The CSDAG to SRDAG transformation led to an unbalanced number of produced and consumed token for an edge
-	// 1006: SRDAG edge creation led to an unbalanced number of produced and consumed token for an edge
-	// 1007: Removing an edge from an empty graph
-	// 1008: Adding a slave when the slave table is full
-	// 1009: Adding a slave type with a too big index
-	// 1010: Adding a production pattern in a full table
-	// 1011: Adding a consumption pattern in a full table
-	// 1012: Adding a variable in an already full variable table
-	// 1013: Too long expression
-	// 1014: Early end of expression after delimiter
-	// 1015: User entered a not existing operator like "//"
-	// 1016: Garbage at the end of the expression 
-	// 1018: Unwanted character in expression
-	// 1019: Unexpected end of expression
-	// 1020: Expression syntax error
-	// 1021: Operator is not recognized
-	// 1022: Operator is not recognized
-	// 1023: Unknown variable
-	// 1024: Unknown timing
-	// 1025: Trying to allocate a buffer on a full memory
-	// 1026: Adding an expression element in a full stack
-	// 1027: Searching a non existing variable while creating a stack
-	// 1028: Unrecognized token
-	// 1029: Adding an expression element in a full queue
-	// 1030: Removing an expression element from an empty queue
-	// 1031: Popping an expression element from an empty stack
-	// 1032: Parser parenthesis mismatch
-	// 1033: Parser parenthesis mismatch. Left parenthesis missing
-	// 1035: Peeking an expression element from an empty stack
-	// 1036: Failure in RPN parsing
-	// 1037: Expression syntax error: wrong variable assignment
-	// 1038: Problem with variable assignment
-	// 1039: Adding a parameter pattern in a full table
-	// 1040: Adding a parameter in a full table
-	// 1041: Incorrect index
-	// 1042: Setting an improper data rate
-	// 1043: Removing a slave from an empty architecture
-	// 1044: The data rate does not fit in table dynamic. Too low rate.
-	// 1045: Error in xmlParser
-	// 1046: Error in Timer
-	// 1047: Too many Input for a Vertex
-	// 1048: Too many Output for a Vertex
-	// 1049: Schedule Check Failed
-	// 1050: Production/Consumption Pattern too long
-	// 1051: Parameter Pattern too long
-	// 1052: Parameters cannot have a pattern
-	// 1053: Try to activate more slaves that the Architecture got
-	// 1054: No more available subgraphs.
-	// 1055: Too many parameters.
-	// 1056: Root vertex not set.
-	// 1057: Too many parameters.
-	// 1058: Too many actor machines.
-	// 1059: Too many actors.
-	// 1060: Too many FIFOs.
-	// 1061: Graph already executed.
-	// 1062: No executable vertices found.
-	// 1063: Multiple SrDAG vertices reference a single PiSDF configure vertex.
-	// 1064: Matching not found for unplugged vertex when merging SrDAG.
-	// 1065: Vertex was deleted.
-	// 1066: Invalid production/consumption value.
-	// 1067: Unexpected amount of data received.
-	// 1068: Unexpected data values received.
-	// 1069: Top's subgraph is missing.
-	// 1070: Top's root vertex is missing.
-	// 1071: Local SDF graph was empty.
-	// 1072: MAX_FILE_NAME_SIZE too small.
-	// 1073: MAX_SLAVE_NAME_SIZE too small.
-	// 1074: EXPR_LEN_MAX too small.
-	// 1075: MAX_VERTEX_NAME_SIZE too small.
-	// 1076: List too small
-
-	printf("\nExiting with code %d\n", exitCode);
+static void exitWithCode(int exitCode, const char* opt = ""){
+	printf("\nExiting with code %d:\n", exitCode);
+	if(exitCode>=2000 && exitCode<2000+MAX_ERROR_COUNT)
+		printf("[%s] %s\n", opt, errorString[exitCode-2000]);
 	exit(exitCode);
 }
 

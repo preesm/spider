f7f7e862c57747ee3471f757dfca1cb01fa8f924
Julien Heulot <jheulot@insa-rennes.fr>
2015-01-06 17:54:24 +0100

    Fix bug in Optims

--- spider/master/spider/graphTransfo/Optims.cpp
+++ spider/master/spider/graphTransfo/Optims.cpp
@@ -48,9 +48,9 @@
 static int reduceJoinJoin(SRDAGGraph* topDag){
 	for(int i=0; i<topDag->getNVertex(); i++){
 		SRDAGVertex* join0 = topDag->getVertex(i);
-		if(join0->getType() == SRDAG_JOIN){
+		if(join0 && join0->getState() != SRDAG_RUN && join0->getType() == SRDAG_JOIN){
 			SRDAGVertex* join1 = join0->getOutEdge(0)->getSnk();
-			if(join1->getType() == SRDAG_JOIN){
+			if(join1 && join1->getType() == SRDAG_JOIN){
 				int nbToAdd = join0->getNConnectedInEdge();
 				int ixToAdd = join0->getOutEdge(0)->getSnkPortIx();
 
@@ -85,9 +85,9 @@ static int reduceJoinJoin(SRDAGGraph* topDag){
 static int reduceForkFork(SRDAGGraph* topDag){
 	for(int i=0; i<topDag->getNVertex(); i++){
 		SRDAGVertex* secFork = topDag->getVertex(i);
-		if(secFork->getType() == SRDAG_FORK){
+		if(secFork && secFork->getType() == SRDAG_FORK){
 			SRDAGVertex* firFork = secFork->getInEdge(0)->getSrc();
-			if(firFork->getType() == SRDAG_FORK){
+			if(firFork && firFork->getState() != SRDAG_RUN && firFork->getType() == SRDAG_FORK){
 				int nbToAdd = secFork->getNConnectedOutEdge();
 				int ixToAdd = secFork->getInEdge(0)->getSrcPortIx();
 
@@ -123,11 +123,11 @@ static int removeBr(SRDAGGraph* topDag){
 	bool result = false;
 	for(int i=0; i<topDag->getNVertex(); i++){
 		SRDAGVertex* br = topDag->getVertex(i);
-		if(br->getType() == SRDAG_BROADCAST){
+		if(br && br->getState() != SRDAG_RUN && br->getType() == SRDAG_BROADCAST){
 			for(int j=0; j<br->getNConnectedOutEdge(); j++){
 				SRDAGEdge* delEdge = br->getOutEdge(j);
 				SRDAGVertex* endVertex = delEdge->getSnk();
-				if(endVertex->getType() == SRDAG_END){
+				if(endVertex && endVertex->getType() == SRDAG_END){
 					int nbOutput = br->getNConnectedOutEdge();
 
 					delEdge->disconnectSrc();
@@ -145,7 +145,7 @@ static int removeBr(SRDAGGraph* topDag){
 				}
 			}
 
-			if(br->getNConnectedOutEdge() == 1){
+			if(br->getNConnectedOutEdge() == 1 && br->getOutEdge(0)->getSnk()){
 				SRDAGEdge* inEdge = br->getInEdge(0);
 				SRDAGVertex* nextVertex = br->getOutEdge(0)->getSnk();
 				int edgeIx = br->getOutEdge(0)->getSnkPortIx();
@@ -178,9 +178,139 @@ static int removeBr(SRDAGGraph* topDag){
 	return result;
 }
 
+static int removeFork(SRDAGGraph* topDag){
+	bool result = false;
+	for(int i=0; i<topDag->getNVertex(); i++){
+		SRDAGVertex* fork = topDag->getVertex(i);
+		if(fork && fork->getState() != SRDAG_RUN && fork->getType() == SRDAG_FORK){
+			for(int j=0; j<fork->getNConnectedOutEdge(); j++){
+				SRDAGEdge* delEdge = fork->getOutEdge(j);
+				SRDAGVertex* nextVertex = delEdge->getSnk();
+				if(nextVertex && delEdge->getRate() == 0){
+					switch (nextVertex->getType()) {
+						case SRDAG_END:{
+							int nbOutput = fork->getNConnectedOutEdge();
+							delEdge->disconnectSrc();
+							delEdge->disconnectSnk();
+							topDag->delEdge(delEdge);
+							topDag->delVertex(nextVertex);
+
+							for(int k=j+1; k<nbOutput; k++){
+								SRDAGEdge *edge = fork->getOutEdge(k);
+								edge->disconnectSrc();
+								edge->connectSrc(fork, k-1);
+							}
+							result = true;
+							break;}
+						case SRDAG_JOIN:{
+							int forkNOut = fork->getNConnectedOutEdge();
+							int joinPortIx = delEdge->getSnkPortIx();
+							int joinNIn = nextVertex->getNConnectedInEdge();
+							delEdge->disconnectSrc();
+							delEdge->disconnectSnk();
+							topDag->delEdge(delEdge);
+
+							/* Shift on Fork */
+							for(int k=j+1; k<forkNOut; k++){
+								SRDAGEdge *edge = fork->getOutEdge(k);
+								edge->disconnectSrc();
+								edge->connectSrc(fork, k-1);
+							}
+
+							/* Shift on Join */
+							for(int k=joinPortIx+1; k<joinNIn; k++){
+								SRDAGEdge *edge = nextVertex->getInEdge(k);
+								edge->disconnectSnk();
+								edge->connectSnk(nextVertex, k-1);
+							}
+
+							break;}
+					}
+				}
+			}
+
+			if(fork->getNConnectedOutEdge() == 1 && fork->getOutEdge(0)->getSnk()){
+				SRDAGEdge* inEdge = fork->getInEdge(0);
+				SRDAGVertex* nextVertex = fork->getOutEdge(0)->getSnk();
+				int edgeIx = fork->getOutEdge(0)->getSnkPortIx();
+
+				SRDAGEdge* delEdge = fork->getOutEdge(0);
+				delEdge->disconnectSrc();
+				delEdge->disconnectSnk();
+				topDag->delEdge(delEdge);
+
+				inEdge->disconnectSnk();
+				inEdge->connectSnk(nextVertex, edgeIx);
+				topDag->delVertex(fork);
+
+				return true;
+			}else if(fork->getNConnectedOutEdge() == 0){
+				SRDAGVertex* end = topDag->addEnd();
+				SRDAGEdge* edge = fork->getInEdge(0);
+
+				edge->disconnectSnk();
+				edge->connectSnk(end, 0);
+
+				topDag->delVertex(fork);
+				return true;
+			}
+
+			if(result)
+				return true;
+		}
+	}
+	return result;
+}
+
+static int removeJoin(SRDAGGraph* topDag){
+	bool result = false;
+	for(int i=0; i<topDag->getNVertex(); i++){
+		SRDAGVertex* join = topDag->getVertex(i);
+		if(join && join->getState() != SRDAG_RUN && join->getType() == SRDAG_JOIN){
+			if(join->getNConnectedOutEdge() == 1 && join->getOutEdge(0)->getSnk()){
+				SRDAGEdge* inEdge = join->getInEdge(0);
+				SRDAGVertex* nextVertex = join->getOutEdge(0)->getSnk();
+				int edgeIx = join->getOutEdge(0)->getSnkPortIx();
+
+				SRDAGEdge* delEdge = join->getOutEdge(0);
+				delEdge->disconnectSrc();
+				delEdge->disconnectSnk();
+				topDag->delEdge(delEdge);
+
+				inEdge->disconnectSnk();
+				inEdge->connectSnk(nextVertex, edgeIx);
+				topDag->delVertex(join);
+
+				return true;
+			}else if(join->getNConnectedOutEdge() == 0){
+				SRDAGVertex* end = topDag->addEnd();
+				SRDAGEdge* edge = join->getInEdge(0);
+
+				edge->disconnectSnk();
+				edge->connectSnk(end, 0);
+
+				topDag->delVertex(join);
+				return true;
+			}
+
+			if(result)
+				return true;
+		}
+	}
+	return result;
+}
+
 void optims(SRDAGGraph *topDag, Stack* stack){
-	while(reduceJoinJoin(topDag));
-	while(reduceForkFork(topDag));
-	while(removeBr(topDag));
+	bool res;
+	topDag->print("before.gv");
+	do{
+		res = false;
+		res = res || reduceJoinJoin(topDag);
+		res = res || reduceForkFork(topDag);
+		res = res || removeBr(topDag);
+		res = res || removeFork(topDag);
+		res = res || removeJoin(topDag);
+	}while(res);
+	topDag->print("after.gv");
 }
 

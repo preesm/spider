1d9f0b58acc1184211f856aafe510195d883362d
Julien Heulot <jheulot@insa-rennes.fr>
2015-04-01 10:00:03 +0200

    Update EDMA code to use 4 channels

--- apps/dac_fft_group_fixed/projects/k2_dsp/src/edma.c
+++ apps/dac_fft_group_fixed/projects/k2_dsp/src/edma.c
@@ -40,7 +40,6 @@
 
 #include "edma.h"
 
-#include <ti/csl/device/k2h/src/cslr_device.h>
 #include <ti/csl/csl_edma3.h>
 #include <ti/csl/csl_edma3Aux.h>
 #include <ti/csl/csl_cacheAux.h>
@@ -55,14 +54,14 @@ CSL_TpccRegs*  gEDMACC0Regs  = (CSL_TpccRegs*)CSL_EDMACC_0_REGS;
 CSL_TpccRegs*  gEDMACC1Regs  = (CSL_TpccRegs*)CSL_EDMACC_1_REGS;
 CSL_TpccRegs*  gEDMACC2Regs  = (CSL_TpccRegs*)CSL_EDMACC_2_REGS;
 
-static CSL_Edma3Handle       	hModule;
+static CSL_Edma3Handle       	hEdma0, hEdma2;
 static CSL_Status               status;
 static CSL_Edma3Context         context;
-static CSL_Edma3Obj             edmaObj;
-static CSL_Edma3ChannelObj      chObj;
+static CSL_Edma3Obj             edmaObj0, edmaObj2;
+static CSL_Edma3ChannelObj      ch0Obj, ch1Obj, ch2Obj, ch3Obj;
 static CSL_Edma3ChannelAttr     chAttr;
-static CSL_Edma3ChannelHandle   hChannel;
-static CSL_Edma3ParamHandle     hParam;
+static CSL_Edma3ChannelHandle   hChannel0, hChannel1, hChannel2, hChannel3;
+static CSL_Edma3ParamHandle     hParam0, hParam1, hParam2, hParam3;
 
 int edma_init(){
     /* Module initialization */
@@ -72,8 +71,14 @@ int edma_init(){
     }
 
     /* Open the EDMA Module using the provided instance number */
-    hModule = CSL_edma3Open(&edmaObj, 0, NULL, &status);
-    if ( (hModule == NULL) || (status != CSL_SOK))
+    hEdma0 = CSL_edma3Open(&edmaObj0, 0, NULL, &status);
+    if ( (hEdma0 == NULL) || (status != CSL_SOK))
+    {
+        printf ("Error: EDMA module open failed\n");
+        return -1;
+    }
+    hEdma2 = CSL_edma3Open(&edmaObj2, 2, NULL, &status);
+    if ( (hEdma0 == NULL) || (status != CSL_SOK))
     {
         printf ("Error: EDMA module open failed\n");
         return -1;
@@ -82,21 +87,64 @@ int edma_init(){
     /* Channel open */
     chAttr.regionNum = CSL_EDMA3_REGION_GLOBAL;
     chAttr.chaNum    = 0;
-    hChannel = CSL_edma3ChannelOpen(&chObj, 0, &chAttr, &status);
-    if ((hChannel == NULL) || (status != CSL_SOK)) 
+    hChannel0 = CSL_edma3ChannelOpen(&ch0Obj, 0, &chAttr, &status);
+    if ((hChannel0 == NULL) || (status != CSL_SOK))
     {
         printf ("Error: Unable to open EDMA Channel:0\n");
         return -1;
     }
+    chAttr.chaNum    = 1;
+    hChannel1 = CSL_edma3ChannelOpen(&ch1Obj, 0, &chAttr, &status);
+    if ((hChannel1 == NULL) || (status != CSL_SOK))
+    {
+        printf ("Error: Unable to open EDMA Channel:1\n");
+        return -1;
+    }
+    chAttr.regionNum = CSL_EDMA3_REGION_GLOBAL;
+    chAttr.chaNum    = 0;
+    hChannel2 = CSL_edma3ChannelOpen(&ch2Obj, 2, &chAttr, &status);
+    if ((hChannel2 == NULL) || (status != CSL_SOK))
+    {
+        printf ("Error: Unable to open EDMA Channel:2\n");
+        return -1;
+    }
+    chAttr.chaNum    = 1;
+    hChannel3 = CSL_edma3ChannelOpen(&ch3Obj, 2, &chAttr, &status);
+    if ((hChannel3 == NULL) || (status != CSL_SOK))
+    {
+        printf ("Error: Unable to open EDMA Channel:3\n");
+        return -1;
+    }
 
     /* Map the DMA Channel to PARAM Block 0. */
-    CSL_edma3MapDMAChannelToParamBlock (hModule, 0, 0);
+    CSL_edma3MapDMAChannelToParamBlock (hEdma0, 0, 0);
+    CSL_edma3MapDMAChannelToParamBlock (hEdma0, 1, 1);
+    CSL_edma3MapDMAChannelToParamBlock (hEdma2, 0, 0);
+    CSL_edma3MapDMAChannelToParamBlock (hEdma2, 1, 1);
 
-    CSL_edma3HwChannelSetupQue(hChannel, CSL_EDMA3_QUE_0);
+    status = CSL_edma3HwChannelSetupQue(hChannel0, CSL_EDMA3_QUE_0);
+    status = CSL_edma3HwChannelSetupQue(hChannel1, CSL_EDMA3_QUE_1);
+    status = CSL_edma3HwChannelSetupQue(hChannel2, CSL_EDMA3_QUE_0);
+    status = CSL_edma3HwChannelSetupQue(hChannel3, CSL_EDMA3_QUE_1);
 
     /* Obtain a handle to parameter set 0 */
-    hParam = CSL_edma3GetParamHandle(hChannel, 0, &status);
-    if (hParam == NULL){
+    hParam0 = CSL_edma3GetParamHandle(hChannel0, 0, &status);
+    if (hParam0 == NULL){
+        printf ("Error: EDMA Get Parameter Entry failed for 2.\n");
+        return -1;
+    }
+    hParam1 = CSL_edma3GetParamHandle(hChannel1, 1, &status);
+    if (hParam1 == NULL){
+        printf ("Error: EDMA Get Parameter Entry failed for 2.\n");
+        return -1;
+    }
+    hParam2 = CSL_edma3GetParamHandle(hChannel2, 0, &status);
+    if (hParam2 == NULL){
+        printf ("Error: EDMA Get Parameter Entry failed for 2.\n");
+        return -1;
+    }
+    hParam3 = CSL_edma3GetParamHandle(hChannel3, 1, &status);
+    if (hParam3 == NULL){
         printf ("Error: EDMA Get Parameter Entry failed for 2.\n");
         return -1;
     }
@@ -106,18 +154,41 @@ int edma_init(){
 
 int edma_clean(){
 	/* Close channel */
-	if (CSL_edma3ChannelClose(hChannel) != CSL_SOK)
+	if (CSL_edma3ChannelClose(hChannel0) != CSL_SOK)
 	{
 		printf("Error: EDMA Channel Close failed\n");
 		return -1;
 	}
 
+    if (CSL_edma3ChannelClose(hChannel1) != CSL_SOK)
+    {
+        printf("Error: EDMA Channel Close failed\n");
+        return -1;
+    }
+
+    if (CSL_edma3ChannelClose(hChannel2) != CSL_SOK)
+    {
+        printf("Error: EDMA Channel Close failed\n");
+        return -1;
+    }
+
+    if (CSL_edma3ChannelClose(hChannel3) != CSL_SOK)
+    {
+        printf("Error: EDMA Channel Close failed\n");
+        return -1;
+    }
+
 	/* Close EDMA module */
-	if (CSL_edma3Close(hModule) != CSL_SOK)
+	if (CSL_edma3Close(hEdma0) != CSL_SOK)
 	{
 		printf("Error: EDMA Module Close failed\n");
 		return -1;
 	}
+    if (CSL_edma3Close(hEdma2) != CSL_SOK)
+    {
+        printf("Error: EDMA Module Close failed\n");
+        return -1;
+    }
 
 	return 0;
 }
@@ -146,38 +217,135 @@ int edma_cpy (
     myParamSetup.srcAddr    = (Uint32)src;
     myParamSetup.dstAddr    = (Uint32)dst;
     myParamSetup.aCntbCnt   = CSL_EDMA3_CNT_MAKE(aCnt,bCnt);
-    myParamSetup.cCnt 		= cCnt;
+    myParamSetup.cCnt 		= cCnt>>2;
     myParamSetup.srcDstBidx = CSL_EDMA3_BIDX_MAKE(srcBIdx,dstBIdx);
-    myParamSetup.srcDstCidx = CSL_EDMA3_CIDX_MAKE(srcCIdx,dstCIdx);
+    myParamSetup.srcDstCidx = CSL_EDMA3_CIDX_MAKE(srcCIdx*4,dstCIdx*4);
     myParamSetup.linkBcntrld= 0xFFFF;
 
     /* Ping setup */
-    if (CSL_edma3ParamSetup(hParam,&myParamSetup) != CSL_SOK)
+    if (CSL_edma3ParamSetup(hParam0,&myParamSetup) != CSL_SOK)
     {
         printf ("Error: EDMA Parameter Entry Setup failed\n");    
         return -1;
     }
     
+    myParamSetup.option = CSL_EDMA3_OPT_MAKE(CSL_EDMA3_ITCCH_EN,
+                                             CSL_EDMA3_TCCH_DIS,
+                                             CSL_EDMA3_ITCINT_DIS,
+                                             CSL_EDMA3_TCINT_EN,
+                                             1,
+                                             CSL_EDMA3_TCC_NORMAL,
+                                             CSL_EDMA3_FIFOWIDTH_NONE,
+                                             CSL_EDMA3_STATIC_DIS,
+                                             CSL_EDMA3_SYNC_AB,
+                                             CSL_EDMA3_ADDRMODE_INCR,
+                                             CSL_EDMA3_ADDRMODE_INCR );
+    myParamSetup.srcAddr    = (((Uint32)src) + (aCnt*bCnt));
+    myParamSetup.dstAddr    = (((Uint32)dst) + (aCnt));
+    myParamSetup.aCntbCnt   = CSL_EDMA3_CNT_MAKE(aCnt,bCnt);
+    myParamSetup.cCnt       = cCnt>>2;
+    myParamSetup.srcDstBidx = CSL_EDMA3_BIDX_MAKE(srcBIdx,dstBIdx);
+    myParamSetup.srcDstCidx = CSL_EDMA3_CIDX_MAKE(srcCIdx*4,dstCIdx*4);
+    myParamSetup.linkBcntrld= 0xFFFF;
+
+    /* Ping setup */
+    if (CSL_edma3ParamSetup(hParam1,&myParamSetup) != CSL_SOK)
+    {
+        printf ("Error: EDMA Parameter Entry Setup failed\n");
+        return -1;
+    }
+
+    myParamSetup.option = CSL_EDMA3_OPT_MAKE(CSL_EDMA3_ITCCH_EN,
+                                             CSL_EDMA3_TCCH_DIS,
+                                             CSL_EDMA3_ITCINT_DIS,
+                                             CSL_EDMA3_TCINT_EN,
+                                             0,
+                                             CSL_EDMA3_TCC_NORMAL,
+                                             CSL_EDMA3_FIFOWIDTH_NONE,
+                                             CSL_EDMA3_STATIC_DIS,
+                                             CSL_EDMA3_SYNC_AB,
+                                             CSL_EDMA3_ADDRMODE_INCR,
+                                             CSL_EDMA3_ADDRMODE_INCR );
+    myParamSetup.srcAddr    = (((Uint32)src) + (2*aCnt*bCnt));
+    myParamSetup.dstAddr    = (((Uint32)dst) + (2*aCnt));
+    myParamSetup.aCntbCnt   = CSL_EDMA3_CNT_MAKE(aCnt,bCnt);
+    myParamSetup.cCnt       = cCnt>>2;
+    myParamSetup.srcDstBidx = CSL_EDMA3_BIDX_MAKE(srcBIdx,dstBIdx);
+    myParamSetup.srcDstCidx = CSL_EDMA3_CIDX_MAKE(srcCIdx*4,dstCIdx*4);
+    myParamSetup.linkBcntrld= 0xFFFF;
+
+    /* Ping setup */
+    if (CSL_edma3ParamSetup(hParam2,&myParamSetup) != CSL_SOK)
+    {
+        printf ("Error: EDMA Parameter Entry Setup failed\n");
+        return -1;
+    }
+
+    myParamSetup.option = CSL_EDMA3_OPT_MAKE(CSL_EDMA3_ITCCH_EN,
+                                             CSL_EDMA3_TCCH_DIS,
+                                             CSL_EDMA3_ITCINT_DIS,
+                                             CSL_EDMA3_TCINT_EN,
+                                             1,
+                                             CSL_EDMA3_TCC_NORMAL,
+                                             CSL_EDMA3_FIFOWIDTH_NONE,
+                                             CSL_EDMA3_STATIC_DIS,
+                                             CSL_EDMA3_SYNC_AB,
+                                             CSL_EDMA3_ADDRMODE_INCR,
+                                             CSL_EDMA3_ADDRMODE_INCR );
+    myParamSetup.srcAddr    = (((Uint32)src) + (3*aCnt*bCnt));
+    myParamSetup.dstAddr    = (((Uint32)dst) + (3*aCnt));
+    myParamSetup.aCntbCnt   = CSL_EDMA3_CNT_MAKE(aCnt,bCnt);
+    myParamSetup.cCnt       = cCnt>>2;
+    myParamSetup.srcDstBidx = CSL_EDMA3_BIDX_MAKE(srcBIdx,dstBIdx);
+    myParamSetup.srcDstCidx = CSL_EDMA3_CIDX_MAKE(srcCIdx*4,dstCIdx*4);
+    myParamSetup.linkBcntrld= 0xFFFF;
+
+    /* Ping setup */
+    if (CSL_edma3ParamSetup(hParam3,&myParamSetup) != CSL_SOK)
+    {
+        printf ("Error: EDMA Parameter Entry Setup failed\n");
+        return -1;
+    }
+
     /* Interrupt enable (Bits 0-1)  for the global region interrupts */
     regionIntr.region = CSL_EDMA3_REGION_GLOBAL;
-    regionIntr.intr   = 0x1;
+    regionIntr.intr   = 0x3;
     regionIntr.intrh  = 0x0000;
-    CSL_edma3HwControl(hModule,CSL_EDMA3_CMD_INTR_ENABLE,&regionIntr);
+    CSL_edma3HwControl(hEdma0,CSL_EDMA3_CMD_INTR_ENABLE,&regionIntr);
+    regionIntr.region = CSL_EDMA3_REGION_GLOBAL;
+    regionIntr.intr   = 0x3;
+    regionIntr.intrh  = 0x0000;
+    CSL_edma3HwControl(hEdma2,CSL_EDMA3_CMD_INTR_ENABLE,&regionIntr);
 
     /* Trigger channel */
-    CSL_edma3HwChannelControl(hChannel,CSL_EDMA3_CMD_CHANNEL_SET,NULL); 
+    CSL_edma3HwChannelControl(hChannel0,CSL_EDMA3_CMD_CHANNEL_SET,NULL);
+    CSL_edma3HwChannelControl(hChannel1,CSL_EDMA3_CMD_CHANNEL_SET,NULL);
+    CSL_edma3HwChannelControl(hChannel2,CSL_EDMA3_CMD_CHANNEL_SET,NULL);
+    CSL_edma3HwChannelControl(hChannel3,CSL_EDMA3_CMD_CHANNEL_SET,NULL);
 
     regionIntr.region = CSL_EDMA3_REGION_GLOBAL;
     regionIntr.intr   = 0;
     regionIntr.intrh  = 0;
 
-    /* Poll on IPR bit 0 */
+    /* Poll on IPR bits 0 and 1 */
     do {
-        CSL_edma3GetHwStatus(hModule,CSL_EDMA3_QUERY_INTRPEND,&regionIntr);
-    } while (!(regionIntr.intr & 0x1));
+        CSL_edma3GetHwStatus(hEdma0,CSL_EDMA3_QUERY_INTRPEND,&regionIntr);
+    } while ((regionIntr.intr & 0x3) != 0x3);
 
     /* Clear the pending bit */
-    CSL_edma3HwControl(hModule,CSL_EDMA3_CMD_INTRPEND_CLEAR,&regionIntr);   
+    CSL_edma3HwControl(hEdma0,CSL_EDMA3_CMD_INTRPEND_CLEAR,&regionIntr);
+
+    regionIntr.region = CSL_EDMA3_REGION_GLOBAL;
+    regionIntr.intr   = 0;
+    regionIntr.intrh  = 0;
+
+    /* Poll on IPR bits 0 and 1 */
+    do {
+        CSL_edma3GetHwStatus(hEdma2,CSL_EDMA3_QUERY_INTRPEND,&regionIntr);
+    } while ((regionIntr.intr & 0x3) != 0x3);
+
+    /* Clear the pending bit */
+    CSL_edma3HwControl(hEdma2,CSL_EDMA3_CMD_INTRPEND_CLEAR,&regionIntr);
 
 
     /* The test passed. */

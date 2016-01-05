1ca9383ce8bd76c9af44ddcb4ed29ea8e145cbfc
Julien Heulot <jheulot@insa-rennes.fr>
2016-01-05 13:21:29 +0100

    move platforms files

--- master/libspider/platforms/platform_linux/LinuxSpiderCommunicator.cpp
+++ master/libspider/platforms/platform_linux/LinuxSpiderCommunicator.cpp
@@ -35,13 +35,12 @@
  ****************************************************************************/
 
 #include "LinuxSpiderCommunicator.h"
+#include <monitor/StackMonitor.h>
 
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-#include <monitor/StackMonitor.h>
 
 LinuxSpiderCommunicator::LinuxSpiderCommunicator(
 		int msgSizeMax,

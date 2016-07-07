f53c5c8c2cde2b9ab6adb3a81a462bab30828318
Julien Heulot <jheulot@insa-rennes.fr>
2016-07-07 09:54:40 +0200

    Release 0.6-1

--- master/build.sh
+++ master/build.sh
@@ -1,10 +1,10 @@
 
-rm libspider_0.5.orig.tar.gz
-tar cf libspider_0.5.orig.tar libspider
-gzip libspider_0.5.orig.tar
-mkdir libspider-0.5
-cp libspider/* libspider-0.5 -R
-cd libspider-0.5/
+rm libspider_0.6.orig.tar.gz
+tar cf libspider_0.6.orig.tar libspider
+gzip libspider_0.6.orig.tar
+mkdir libspider-0.6
+cp libspider/* libspider-0.6 -R
+cd libspider-0.6/
 debuild -S -sa --lintian-opts -i
 cd ..
-sudo pbuilder build libspider_0.5-1.dsc
+sudo pbuilder build libspider_0.6-1.dsc

004e98d0e74e5d202fd889823c43cbb7827211d9
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-05-17 15:39:04 +0200

    (Pthread) Disable cpu affinity on macos

--- master/libspider/platforms/platform_pthread/platformPThread.cpp
+++ master/libspider/platforms/platform_pthread/platformPThread.cpp
@@ -82,6 +82,9 @@ static void setAffinity(int cpuId) {
 #ifdef WIN32
     fprintf(stdout, "CPU affinity is not supported on Windows platforms. Ignoring argument %d.\n", cpuId);
 #else
+#ifdef __APPLE__
+    printf("WARNING: Affinity is not supported on MacOS. Argument %d ignored.\n", cpuId);
+#else
     cpu_set_t mask;
     int status;
 
@@ -91,6 +94,7 @@ static void setAffinity(int cpuId) {
     if (status != 0) {
         perror("sched_setaffinity");
     }
+#endif
 #endif
 }
 
